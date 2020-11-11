244542723c839aa72ca59b52a4a2b00488bd5249
Edgar <edgarmbarr@gmail.com>
2020-11-11 09:49:18 -0800

    Got testing and prompter back

--- src/com/backenders/clue/Game.java
+++ src/com/backenders/clue/Game.java
@@ -46,18 +46,12 @@ public class Game {
     }
     private void generateGame() throws IOException {
         hp.setCurrentRoom(RoomType.BALLROOM);
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
         prompter.info("Welcome to clue");
         prompter.promptPause();
         prompter.info("A crazy mystery game its pretty cool");
         prompter.promptPause();
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
     }
 
     private Guess askPlayerGuess(){
