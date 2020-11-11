5acc9656145c375d9b91ea16411f6f9a46c91a8f
Gina Villegas <ginayvillegas@gmail.com>
2020-11-11 08:58:50 -0800

    solution, player and roleplayer changes

--- src/com/backenders/clue/RolePlayer.java
+++ src/com/backenders/clue/RolePlayer.java
@@ -11,42 +11,50 @@ public enum RolePlayer {
     MRS_ORCHID("Mrs. Orchid", "Gentle and sensitive but bold. Watch for her reaction."),
     COLONEL_MUSTARD("Colonel Mustard", "Sharp and stern. He expects you to do what he orders."),
     MR_GREEN("Mr. Green", "Environmentally conscious and knowledgeable. He will have a better method."),
-    //PROFESSOR_PLUM("Professor Plum", "Timid and introverted. He is often reading so doesn't notice much."),
+    LIEUTENANT_DAN("Lieutenant Dan", "Courageous and curious. He is worldly and confident."),
     MISS_SCARLET("Miss Scarlet", "Deceptively quiet and sweet. You don't realize her strength."),
     CAPTAIN_CANDACE("Captain Candace", "Brave, thoughtful leader. Look to her for insight AND direction."),
     PROFESSOR_EDGAR("Professor Edgar", "Intelligent, creative and soft-spoken. Rockstar of code review. His instruction is invaluable."),
     GEMOLOGIST_GINA("Gemologist Gina", "Curious and a little odd. She can spot a fake.");
 
-    private static EnumSet<RolePlayer> rolePlayers;
+    protected static EnumSet<RolePlayer> rolePlayers;
 
     private String charDescription;
     private String rpName;
 
     // Constructor
-    RolePlayer(String rpName, String charDescription){
+    RolePlayer(String rpName, String charDescription) {
         this.rpName = rpName;
         this.charDescription = charDescription;
     }
 
-    public static Stream<RolePlayer> rpList(){
+    static Stream<RolePlayer> rpList() {
         return Stream.of(RolePlayer.values());
     }
 
-    public static void printRolePlayerList() {
+    public void printRolePlayerList(Stream<RolePlayer> rpList) {
         RolePlayer.rpList()
-                .forEach(System.out::println);
+                .forEach(RolePlayer -> System.out.println(rpName + " " + charDescription));
 
         // Or we can use an EnumSet because it is supposed to be faster
         // and better than a map....
 //        EnumSet.allOf(RolePlayer.class)
 //                .forEach(System.out::println);
     }
+
+    static EnumSet<RolePlayer> printRolePlayers() {
+        EnumSet<RolePlayer> rpList = EnumSet.allOf(RolePlayer.class);
+        System.out.println(rpList);
+        return rpList;
+    }
+
     // Accessor Methods
     public String getCharDescription() {
         return charDescription;
     }
 
-   public String getRpName() {
+    public String getRpName() {
         return rpName;
     }
+
 }
